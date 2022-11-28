cc7b4e360e07e4a9a6f84f79f146ec3e6db52d01
Mohammed Lathiya <71916101+Mohammed-lathiya@users.noreply.github.com>
2022-11-28 16:41:33 +0530

    Create acf-option-repeater.php

---
+++ acf-option-repeater.php
@@ -0,0 +1,9 @@
+<?php
+
+if( !class_exists('acf_plugin_repeater') )
+include_once('acf-repeater/acf-repeater.php');
+
+if( !class_exists('acf_options_page') )
+include_once('acf-options-page/acf-options-page.php');
+
+?>
